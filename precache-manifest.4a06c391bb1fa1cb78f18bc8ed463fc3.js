5921cc46ebcc8e6251e0b0d4c35974fbab312dd3
josipoviciwan <josipoviciwan@gmail.com>
2020-07-20 15:07:11 +0200

    Updates

---
+++ precache-manifest.4a06c391bb1fa1cb78f18bc8ed463fc3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b79a4d57fd83b28700a129b171a9ada",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "23fcc1490e9765c3cab0",
+    "url": "/portfolio/static/css/main.dba187d3.chunk.css"
+  },
+  {
+    "revision": "c8b07144538b54a5accd",
+    "url": "/portfolio/static/js/2.3c44a60c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3c44a60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fcc1490e9765c3cab0",
+    "url": "/portfolio/static/js/main.d969f73a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ae7e626dd81c6f21da3871d0e250f4e0",
+    "url": "/portfolio/static/media/cobe.ae7e626d.png"
+  },
+  {
+    "revision": "328728fa5b6b6bfb6de3caf15b47d7af",
+    "url": "/portfolio/static/media/fontello.328728fa.woff"
+  },
+  {
+    "revision": "721f121f2c76e5394c2ba418877a5719",
+    "url": "/portfolio/static/media/gignums.721f121f.png"
+  },
+  {
+    "revision": "ce2d5aebd72cd6bf54fe51d596a0b474",
+    "url": "/portfolio/static/media/ieee.ce2d5aeb.jpg"
+  },
+  {
+    "revision": "154629f25d417c8c0a4ac3a83707979a",
+    "url": "/portfolio/static/media/linkedin.154629f2.svg"
+  },
+  {
+    "revision": "ac710483dc876abe436315cbdd250183",
+    "url": "/portfolio/static/media/lyrx.ac710483.png"
+  },
+  {
+    "revision": "471661ad628500d8e0d7dd67c5ce67c2",
+    "url": "/portfolio/static/media/profile.471661ad.png"
+  },
+  {
+    "revision": "f1694f1df243fc750783060b8244b6f9",
+    "url": "/portfolio/static/media/skillvalue.f1694f1d.png"
+  }
+]);
